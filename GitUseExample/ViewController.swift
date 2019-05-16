336ae20fcf7935f3ca7596f56185f4827ea53abd
Vernon Williams <vernonw@panix.com>
2019-05-16 09:43:28 -0400

    Initial Commit

---
+++ GitUseExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitUseExample
+//
+//  Created by Vernon  Williams on 5/16/19.
+//  Copyright © 2019 Vernon  Williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
